bfd954432aba77b364e94e8babea8cedf8555241
Yinusa <yamusa2@uic.edu>
2020-10-30 09:13:45 -0500

    excellent

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
 
+const userRouter = require('./users/userRouter.js')
+const postRouter = require("./posts/postRouter.js")
 const server = express();
+
+server.use(express.json())
+server.use(userRouter);
+server.use(postRouter);
+server.use(logger)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
@@ -8,6 +15,14 @@ server.get('/', (req, res) => {
 
 //custom middleware
 
-function logger(req, res, next) {}
+function logger(req, res, next) {
+  console.log(req.method," ",req.hostname, new Date());
+  next()
+}
+
+
+
+
+
 
 module.exports = server;
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,130 @@
+
 const express = require('express');
+const User = require('./userDb.js');
+const Post = require('../posts/postDb')
+const {use} = require("../posts/postRouter");
+const { getUserPosts } = require('./userDb.js');
+
+
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
+router.post('/api/users',[validateUser],(req, res) => {
   // do your magic!
+  User.insert(req.body)
+  .then(data=>{
+    res.status(201).json({sucessmessage:`sucessfully inserted ${data}`})
+  })
+  .catch(error=>{
+    res.status(401).json({message:"hmmmm"})
+  })
 });
 
-router.post('/:id/posts', (req, res) => {
+router.post('/api/users/:id/posts',[validateUserId,validatePost] ,(req, res) => {
   // do your magic!
+  const aPost = {
+    user_id : req.user.id,
+    text: req.body.text,
+  };
+  Post.insert(aPost)
+  .then(data=>{
+    res.status(200).json(data)
+  })
+  .catch(error=>{
+    res.status(401).json({message:"hmmmm"})
+  })
 });
 
-router.get('/', (req, res) => {
+router.get('/api/users', (req, res) => {
   // do your magic!
+  User.get()
+  .then((user)=>{
+    res.status(200).json(user);
+  })
+  .catch((error)=>{
+    console.log(error)
+    res.status(500).json({errorMessage:"NO such user"})
+  })
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/api/users/:id',[validateUserId],(req, res) => {
+ res.status(201).json(req.user)
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/api/users/:id/posts',[validateUserId], (req, res) => {
   // do your magic!
+  User.getUserPosts(req.user.id)
+  .then((data)=>{
+    res.status(200).json(data)
+})
+  .catch((error)=>{
+    console.log(error)
+    res.status(500).json({errorMessage:"NO such user"})
+  })
+  
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/api/users/:id',[validateUserId], (req, res) => {
   // do your magic!
+  User.remove(req.user.id)
+  .then(data=>{
+    res.status(201).json({Message:`successfully deleted ${data}`})
+  })
+  .catch(error=>{
+    res.status(400).json({errorMessage:"not found"})
+  })
+
 });
 
-router.put('/:id', (req, res) => {
+router.put('/api/users/:id',[validateUserId, validateUser], (req, res) => {
   // do your magic!
+  User.update(req.user.id,req.body)
+  .then(
+    res.status(200).json({message:"success"})
+  )
+  .catch(error=>{
+    res.status(400).json({errorMessage:"not found"})
+  })
 });
 
 //custom middleware
 
 function validateUserId(req, res, next) {
-  // do your magic!
+  const { id } = req.params;
+  User.getById(id)
+
+  .then(data=>{
+    if(data){
+    req.user = data
+    next()
+    }
+    else{
+      res.status(404).json({errorMessage:"no hub with id"+ id})
+    }
+  })
+  .catch(error=>{
+    console.log(error.message)
+  })
 }
 
 function validateUser(req, res, next) {
   // do your magic!
+  if(!req.body || !req.body.name){
+    res.status(401).json({message:"Missing some key elements NAME"})
+  }
+  else{
+    next()
+  }
+
 }
 
 function validatePost(req, res, next) {
   // do your magic!
+  if(!req.body || !req.body.post){
+    res.status(401).json({message:"Missing some key elements NAME"})
 }
-
+else{
+  next()
+}
+}
 module.exports = router;
